f028c3f1c52d7bb9d927aee9805e4305c0d8d67d
Rob <126196331+robagraham808@users.noreply.github.com>
2023-08-10 18:21:12 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const CLI = require("./lib/cli.js");
+
+CLI();
+
+// 1. set up initial file structure
+
+// 2. set up classes for shape, circle, triangle, square
+
+// 3. prompt user with questions
+// ensure to validate that text is 3 characters max
+
+// 4. .then statement to create a new shape
+
+// 5. pass data to a function to make svg
+
+// 6. write test to check that correct shape is generated
